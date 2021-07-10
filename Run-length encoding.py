2fc16be97c5fa7195b8cce02956d8ad4aa81a4d4
KhresnaPanduI <khresna.pandu10@gmail.com>
2021-07-10 17:55:59 +0700

    Final encryption algorithm

--- Run-length encoding.py
+++ Run-length encoding.py
@@ -16,18 +16,21 @@ You can assume the string to be decoded is valid.
 def encrypt(text):
     ciphertext = ''
     temp = 1
-    print(len(text))
-    print(len(text) - 1)
-    for i in range(len(text) - 1):
-        if text[i] == text[i+1]:
+
+    for i in range(len(text)):
+        if i+1 <= len(text)-1 and text[i] == text[i+1]: #count iteration of char
             temp += 1
-        else:
+        elif i < len(text)-1: #add string to ciphertext
             ciphertext += str(temp)
             ciphertext += text[i]
             temp = 1
+        else: #for last char
+            ciphertext += str(temp)
+            ciphertext += text[i]
+
     return ciphertext
 
 a = "AAAABBBCCDAA"
-print(len(a))
 print(encrypt(a))
+
 
